28e3b78db8c5b7f050309f7ee39d625add8bc050
Dilip <dilippoudel578@gmail.com>
2023-01-06 22:59:33 +0200

    Extracted state to own component

--- src/components/LoginForm/LoginForm.js
+++ src/components/LoginForm/LoginForm.js
@@ -1,25 +1,32 @@
-const LoginForm = ({
-  handleSubmit,
-  handleUsernameChange,
-  handlePasswordChange,
-  username,
-  password,
-}) => {
+import { useState } from 'react'
+const LoginForm = ({ handleSubmit }) => {
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const onSubmitLoginForm = (e) => {
+    e.preventDefault()
+    handleSubmit(username, password)
+    setUsername('')
+    setPassword('')
+  }
   return (
     <div>
       <h2>Login</h2>
 
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={onSubmitLoginForm}>
         <div>
           username
-          <input type="text" value={username} onChange={handleUsernameChange} />
+          <input
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
         </div>
         <div>
           password
           <input
             type="password"
             value={password}
-            onChange={handlePasswordChange}
+            onChange={(e) => setPassword(e.target.value)}
           />
         </div>
         <button type="submit">login</button>

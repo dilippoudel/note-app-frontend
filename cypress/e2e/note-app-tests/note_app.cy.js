53e239a62e7ba946fd1644e26c79c29e4f930e55
Dilip <dilippoudel578@gmail.com>
2023-01-14 21:05:47 +0200

    Add custome command for testing

--- cypress/e2e/note-app-tests/note_app.cy.js
+++ cypress/e2e/note-app-tests/note_app.cy.js
@@ -15,7 +15,6 @@ describe('Note app', function () {
       'Note app, Department of Computer Science, University of Helsinki 2022'
     )
   })
-
   describe('when logged in', function () {
     beforeEach(function () {
       cy.contains('Log in').click()
@@ -23,13 +22,16 @@ describe('Note app', function () {
       cy.get('#password').type('dilip123')
       cy.get('#login-button').click()
     })
+
     it('user can login', function () {
       cy.contains('Dilip Poudel logged in')
     })
+
     it('a new note can be created', function () {
-      cy.contains('new note').click()
+      cy.contains('create new note').click()
       cy.get('input').type('a note created by cypress')
       cy.contains('Submit').click()
+
       cy.contains('a note created by cypress')
     })
     describe('and a note exixts', function () {
@@ -43,5 +45,17 @@ describe('Note app', function () {
         cy.contains('another note cypress').contains('make not important')
       })
     })
+  })
+  it('login fails with wrong credentials', function () {
+    // cy.contains('Log in').click()
+    // cy.get('#username').type('dilip123')
+    // cy.get('#password').type('wrong')
+    // cy.get('#login-button').click()
+    cy.login({ username: 'dilip123', password: 'wrong' })
+    cy.get('.error')
+      .should('contain', 'error in username or password')
+      .and('have.css', 'color', 'rgb(255, 0, 0)')
+      .and('have.css', 'border-style', 'solid')
+    cy.get('html').should('not.contain', 'Dilip Poudel logged in')
   })
 })
